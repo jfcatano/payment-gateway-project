8465aab4a72caa0b309b88bdd87236c4fa573ad7
Juansitoh <juanfergamer77@gmail.com>
2025-04-14 13:25:37 -0500

    nest build files

--- backend/dist/prisma/seed.js
+++ backend/dist/prisma/seed.js
@@ -8,25 +8,25 @@ async function main() {
     console.log('Deleted existing products.');
     await prisma.product.create({
         data: {
-            name: 'Awesome Gadget',
-            description: 'The most awesome gadget you will ever need.',
-            price: 99.99,
+            name: 'iPhone 16 Pro',
+            description: 'The most advanced iPhone actually.',
+            price: 5000000,
             stock: 50,
         },
     });
     await prisma.product.create({
         data: {
-            name: 'Super Widget',
-            description: 'A widget that surpasses all others.',
-            price: 45.50,
+            name: 'Air Pods Pro 3',
+            description: 'The best sound quality in the market.',
+            price: 750000,
             stock: 120,
         },
     });
     await prisma.product.create({
         data: {
-            name: 'Hyper Component',
-            description: 'Component for advanced users.',
-            price: 199.00,
+            name: 'Samsung Galaxy S24',
+            description: 'The best Android phone in the market.',
+            price: 6299000,
             stock: 15,
         },
     });
